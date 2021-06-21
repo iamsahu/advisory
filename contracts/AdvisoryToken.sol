22c1e78b25e35417a336698be045ad9b46919645
iamsahu <praffulsahu@gmail.com>
2021-06-21 19:32:55 +0530

    Added staking & withdraw function

--- contracts/AdvisoryToken.sol
+++ contracts/AdvisoryToken.sol
@@ -9,6 +9,9 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract AdvisoryToken is ERC20, Ownable {
     using SafeMath for uint256;
     uint256 public lastClaimedAt;
+
+    mapping(address=>uint256) stakeMapping;
+
     constructor() ERC20("AdvisoryToken", "ADT") {
         _mint(msg.sender, 100000 * 10 ** decimals());
         lastClaimedAt = block.timestamp;
@@ -19,4 +22,21 @@ contract AdvisoryToken is ERC20, Ownable {
         _mint(msg.sender, hoursPassed * 10 ** decimals());
         lastClaimedAt = block.timestamp;
     }
+
+    function stake() public payable {
+        _mint(msg.sender,msg.value/1000);
+        stakeMapping[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amount) external{
+        require(stakeMapping[msg.sender]*1000>=amount,"You don't own that much token");
+        (bool sent, bytes memory data) = msg.sender.call{value: amount*1000}("");
+        require(sent, "Failed to send Ether");
+        stakeMapping[msg.sender] -= amount*1000;
+
+        //Burning
+        _burn(msg.sender,amount);
+    }
+
+    
 }
